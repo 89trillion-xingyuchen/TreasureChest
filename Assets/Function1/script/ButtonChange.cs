c5c819bb489206b0f957fc5fc5555da08a688fe9
89trillion-xingyuchen <yuchen@89trillion.com>
2021-11-11 11:22:43 +0800

    first push

---
+++ Assets/Function1/script/ButtonChange.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonChange : MonoBehaviour
+{
+    //点击事件，跳转界面
+    public void Click()
+    {
+        Debug.Log("点击");
+        SceneManager.LoadScene("Scenes/FrontPage");
+    }
+}
